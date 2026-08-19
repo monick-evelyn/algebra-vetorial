40fe5004ac541f2f2fcf9bc73823f500c1bc2197
MiguelLuan <br.miguelcurseiro1000@gmail.com>
2026-08-19 14:49:28 -0300

    feature: implementa metodo de validacao para vetores nulo no sistema

--- calculadoraAlgebraLinear/src/sistema/SistemaCalculadora.java
+++ calculadoraAlgebraLinear/src/sistema/SistemaCalculadora.java
@@ -2,7 +2,15 @@ package sistema;
 import model.*;
 
 public class SistemaCalculadora {
+	public void validarVetorNulo(Vetor vetor) throws IllegalArgumentException {
+		if(vetor == null) {
+			throw new IllegalArgumentException("Vetor nulo é considerado inválido.");
+		}
+	}
 	public Vetor3D calcularProdutoVetorial(Vetor3D vetorA, Vetor3D vetorB) throws IllegalArgumentException{
+		
+		validarVetorNulo(vetorA);
+		validarVetorNulo(vetorB);
 		
 		double x = vetorA.getY() * vetorB.getZ() - vetorA.getZ() * vetorB.getY();
 		double y = vetorA.getZ() * vetorB.getX() - vetorA.getX() * vetorB.getZ();
