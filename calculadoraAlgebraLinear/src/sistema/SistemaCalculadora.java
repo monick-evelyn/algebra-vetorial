04d4d74dee96a7d872eb863189664dfd59381f78
MiguelLuan <br.miguelcurseiro1000@gmail.com>
2026-08-19 14:44:31 -0300

    refactor: remove static do método calcularProdutoVetorial

--- calculadoraAlgebraLinear/src/sistema/SistemaCalculadora.java
+++ calculadoraAlgebraLinear/src/sistema/SistemaCalculadora.java
@@ -2,8 +2,7 @@ package sistema;
 import model.*;
 
 public class SistemaCalculadora {
-	//public void validarVetorVazio(Vetor vetor)
-	public static Vetor3D calcularProdutoVetorial(Vetor3D vetorA, Vetor3D vetorB) throws IllegalArgumentException{
+	public Vetor3D calcularProdutoVetorial(Vetor3D vetorA, Vetor3D vetorB) throws IllegalArgumentException{
 		
 		double x = vetorA.getY() * vetorB.getZ() - vetorA.getZ() * vetorB.getY();
 		double y = vetorA.getZ() * vetorB.getX() - vetorA.getX() * vetorB.getZ();
@@ -13,14 +12,5 @@ public class SistemaCalculadora {
 		
 		return novoVetor;
 		
-	}
-	
-	public static void main(String[] args) {
-		Vetor3D vetorA = new Vetor3D(1,2,3);
-		Vetor3D vetorB = new Vetor3D(4,5,6);
-		
-		Vetor3D novoVetor = calcularProdutoVetorial(vetorA, vetorB);
-		
-		System.out.println(novoVetor.toString());
 	}
 }
