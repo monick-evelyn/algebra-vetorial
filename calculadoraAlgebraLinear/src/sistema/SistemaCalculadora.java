76500134a234a7c8d931498322cd36b5a7f94a78
Carlos Henrique <www.carlos4k@gmail.com>
2026-08-19 14:12:12 -0300

    feat: add metodos de produto escalar e descobrir angulo

--- calculadoraAlgebraLinear/src/sistema/SistemaCalculadora.java
+++ calculadoraAlgebraLinear/src/sistema/SistemaCalculadora.java
@@ -2,4 +2,34 @@ package sistema;
 
 public class SistemaCalculadora {
 
+	
+	public double produtoEscalar(Vetor2D vetorA, Vetor2D vetorB) {
+		double produto = vetorA.getX() * vetorB.getX() + vetorA.getY() * vetorB.getY();
+		return produto;
+	}
+
+	public double produtoEscalar(Vetor3D vetorA, Vetor3D vetorB) {
+		double produto = vetorA.getX() * vetorB.getX() + vetorA.getY() * vetorB.getY() + vetorA.getZ() * vetorB.getZ();
+		return produto;
+	}
+
+	public double anguloEntreVetores(Vetor2D vetorA, Vetor2D vetorB) {
+		double cosseno = produtoEscalar(vetorA, vetorB) / (vetorA.modulo() * vetorB.modulo());
+		double angulo = Math.acos(cosseno);
+		return angulo;
+	}
+
+	public double anguloEntreVetores(Vetor3D vetorA, Vetor3D vetorB) {
+		double cosseno = produtoEscalar(vetorA, vetorB) / (vetorA.modulo() * vetorB.modulo());
+		double angulo = Math.acos(cosseno);
+		return angulo;
+	}
+
+	public boolean saoPerpendiculares(Vetor2D vetorA, Vetor2D vetorB) {
+		return produtoEscalar(vetorA, vetorB) == 0;
+	}
+
+	public boolean saoPerpendiculares(Vetor3D vetorA, Vetor3D vetorB) {
+		return produtoEscalar(vetorA, vetorB) == 0;
+	}
 }
