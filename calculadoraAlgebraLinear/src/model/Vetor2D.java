2506d9c8e06efb9f8a5bc23436b58642c65e337a
Monick Évelyn <monick.evelyn.tech@gmail.com>
2026-08-19 02:38:28 -0300

    feat: vetores

--- calculadoraAlgebraLinear/src/model/Vetor2D.java
+++ calculadoraAlgebraLinear/src/model/Vetor2D.java
@@ -1,5 +1,30 @@
 package model;
 
 public class Vetor2D {
+	private double x;
+	private double y;
+	
+	public Vetor2D(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+	
+	// Fórmula: raiz_quadrada(x² + y² + z²)
+	public double modulo() {
+		 double modulo = Math.sqrt(x*x + y*y);
+		 return modulo;
+	}
+
+	@Override
+	public String toString() {
+		return "Vetor2D [" + x + ", " + y + "]";
+	}
 }
--- calculadoraAlgebraLinear/src/model/Vetor3D.java
+++ calculadoraAlgebraLinear/src/model/Vetor3D.java
@@ -1,5 +1,38 @@
 package model;
 
 public class Vetor3D {
+	private double x;
+	private double y;
+	private double z;
+	
+	public Vetor3D(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+	
+	// Fórmula: raiz_quadrada(x² + y² + z²)
+	public double modulo() {
+		 double modulo = Math.sqrt(x*x + y*y + z*z);
+		 return modulo;
+	}
+
+	@Override
+	public String toString() {
+		return "Vetor3D [" + x + ", " + y + ", " + z + "]";
+	}
+	
+	
 }
