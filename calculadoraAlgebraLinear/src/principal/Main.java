4ee4ab891a652065675ec73a8a93852f099f5620
Marcos Ruan <113126971+Marcosruan@users.noreply.github.com>
2026-08-20 14:33:35 -0300

    Fix: adiona String[] args faltante no main

--- calculadoraAlgebraLinear/src/principal/Main.java
+++ calculadoraAlgebraLinear/src/principal/Main.java
@@ -4,7 +4,7 @@ import console.ConsoleSistema;
 
 public class Main {
 
-    public static void main() {
+    public static void main(String[] args) {
 
         ConsoleSistema console = new ConsoleSistema();
 
